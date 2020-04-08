6c64a0f95575c42e432a513da10372a658af578f
Crowdbotics <team@crowdbotics.com>
2020-04-08 13:46:03 +0000

    Update models

--- backend/minnesota_animal_se_15592/wsgi.py
+++ backend/minnesota_animal_se_15592/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'minnesota_animal_se_15592.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "minnesota_animal_se_15592.settings")
 
 application = get_wsgi_application()
